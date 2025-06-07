3e926f2bc0089d06b3a3a5fa895bd394693067ac
SophiaChirskaya <sophia.chirskaya@gmail.com>
2025-06-07 14:02:34 +0200

    [feat] back-office

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function index() {
+         $user = Auth::user();
+         return $user->id;
+    }
+
+    public function profile () {
+        return "pagina profile back-office";
+    }
 }
