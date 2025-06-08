f093e2d2fd8ef3f1b51525d171a9a15e1ca915b5
SophiaChirskaya <sophia.chirskaya@gmail.com>
2025-06-08 11:13:05 +0200

    [feat]card.blade.php

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,16 @@
+@props(['book'])
+
+<div class="card h-100 shadow-sm">
+    @if ($book->image)
+    <img src="{{ asset('storage/' . $book->image) }}" alt="{{ $book->title }}" class="card-img-top card-image">
+    @else
+    <img src="{{ asset('images/default.jpg') }}" alt="Immagine non disponibile" class="card-img-top card-image">
+    @endif
+    <div class="card-body text-center">
+        <h5 class="card-title">{{ $book->title }}</h5>
+        <p class="card-text"><strong>Autore:</strong>{{ $book->author }}</p>
+        <p class="card-text"><strong>Anno:</strong>{{ $book->year }}</p>
+
+    </div>
+
+</div>
