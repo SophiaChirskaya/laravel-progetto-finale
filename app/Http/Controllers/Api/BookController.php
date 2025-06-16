a1387757a916a17fe10b662d1cd1f54b791296fb
SophiaChirskaya <sophia.chirskaya@gmail.com>
2025-06-16 15:36:15 +0200

    [fix]api index & show

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Database\Seeders\BooksTableSeeder;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index() {
+
+        $books = Book::with('genre')->get();
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $books
+                ]
+            );
+    }
+
+    public function show(Book $book) {
+
+        $book->load('genre', 'types');
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $book
+            ]
+            );
+    }
+
 }
